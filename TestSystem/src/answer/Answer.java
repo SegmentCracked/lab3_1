9c6d237223ca4593a332547ba61eda5a97a2471f
Mengxiao Lin <linmx0130@163.com>
2016-05-27 23:55:19 +0800

    modify .gitignore

---
+++ TestSystem/src/answer/Answer.java
@@ -0,0 +1,11 @@
+package answer;
+
+public interface Answer {
+	
+	
+	public String getAnswer();
+	public void setAnswer(String aswser);
+	public String writeAnswer();
+	public int getType();
+	public boolean match(Answer answer);
+}
